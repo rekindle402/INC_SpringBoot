515d0df241386884fe444a4584215392dd73bca9
rekindle <soapt@kakao.com>
2025-08-07 11:35:58 +0900

    init:  최초커밋

---
+++ src/main/java/com/sinse/restapp/BoardController.java
@@ -0,0 +1,14 @@
+package com.sinse.restapp;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class BoardController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "JH";
+    }
+}
